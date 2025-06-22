9c304843259066e8b8b60e737cf3cbf608549c05
Pablo Salcedo <pablo.salcedom@gmail.com>
2025-06-21 18:52:21 -0600

    first part of POM workshop

---
+++ src/tests/simple.tests.js
@@ -0,0 +1,54 @@
+describe('Doctors page', () => {
+    beforeEach( async () => {
+        await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
+    });
+
+    it('Check page title', async () => {
+        /* const title = await browser.getTitle()
+        console.log(title);
+
+            if (title !== 'Appointment Planner - Syncfusion Angular Components Showcase App') {
+            throw new Error('Page title isn"t correct')
+        } */
+       await expect(browser).toHaveTitle('Appointment Planner - Syncfusion Angular Components Showcase App');
+    });
+
+    it('Open modal window for adding a new doctor', async () => {
+        // click on doctors item in the side menu
+        await $('[routerlink="/doctors"]').click();
+        // click on add new doctor btn
+        await $('.specialization-types button.e-control').click();
+        // check that a model window is displayed
+        await expect($('.new-doctor-dialog')).toBeDisplayed();
+    });
+
+    it('Add a new doctor', async () => {
+        // click on doctors item in the side menu
+        await $('[routerlink="/doctors"]').click();
+        // click on add new doctor btn
+        await $('.specialization-types button.e-control').click();
+        // wait for visibility of modal window
+        await $('.new-doctor-dialog').waitForDisplayed();
+
+        await $('[name="Name"]').setValue('John Doe');
+        await $('#DoctorMobile').setValue('11111111111111');
+        await $('[name="Email"]').setValue('test@test.com');
+        await $('[name="Education"]').setValue('Basic');
+        await $('[name="Designation"]').setValue('Test');
+
+        await $('.e-footer-content button.e-primary').click();
+
+        await expect($('.new-doctor-dialog')).not.toBeDisplayed();
+
+        await expect($('#Specialist_8').$('.name')).toHaveText('Dr. John Doe');
+        await expect($('#Specialist_8').$('.education')).toHaveText('Basic', {ignoreCase: true}); // ignoreCase will ignore lower and upper case.
+    });
+
+    it('Close a modal window for adding a new doctor', async () => {
+        await $('[routerlink="/doctors"]').click();
+        await $('.specialization-types button.e-control').click();
+        await $('.new-doctor-dialog').waitForDisplayed();
+        await $('.new-doctor-dialog .e-dlg-closeicon-btn').click();
+        await expect($('.new-doctor-dialog')).not.toBeDisplayed();
+    });
+})
